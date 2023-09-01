fcba1ee2df85f1906440b3e669a64abec74101da
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-01 08:29:28 +0300

    update

--- contacts.js
+++ contacts.js
@@ -20,23 +20,22 @@ async function listContacts() {
 async function getContactById(contactId) {
     const contacts = await listContacts();
     const oneContact = contacts.find((contact) => contact.id === contactId);
-    return null || oneContact;
+    return oneContact || null;
   
 }
 
 async function removeContact(contactId) {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((contact) => contact.id === contactId);
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
 
-    if (index === -1) {
-        return null;
-    }
+  if (index === -1) {
+    return null;
+  }
 
-    const deletedContact = contacts.splice(index, 1)[0];
-    await listContacts(contacts);
+  const deletedContact = contacts.splice(index, 1)[0];
+  await listContacts(contacts);
 
-    return deletedContact;
-  
+  return deletedContact;
 }
 
 async function addContact(name, email, phone) {
